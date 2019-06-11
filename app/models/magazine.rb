548654f1b1e3f3f54efec02cb68ebd6f3b063748
Elma Rahman <elmarahman635@gmail.com>
2019-06-11 11:40:10 -0400

    I did itgit add .git add .git add .git add .

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,59 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
 
+    @@all << self
   end
 
+#gets back article for this magazine
+def my_article
+  Article.all.select do |article|
+    article.magazine == self
+    binding.pry
+  end
+end
+
+#gets back author for this magazine
+  def my_author
+    self.my_article.map do |article|
+      article.author
+    end
+  end
+
+#works
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name
+  end
+end
+
+#works
+def article_titles
+  self.my_article.map do |article|
+    article.title
+  end
+end
+
+def contributors
+  self.my_article.map do |article|
+    article.author
+  end
+end
+
+  def word_count
+    self.my_article.reduce(0) do |result, article|
+      result + article.content
+    end
+  end
+
+
+  def self.all
+    @@all
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,13 +7,34 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+a1 = Author.new("Elma")
+a2 = Author.new("Lamia")
+a3 = Author.new("Andy")
+a4 = Author.new("James")
 
 
+m1 = Magazine.new("Time New York", "News")
+m2 = Magazine.new("Buzzfeed", "fun comical news")
+m3 = Magazine.new("Vogue", "fashion")
+m4 = Magazine.new("Cosmo", "gossip")
 
+ar1 = Article.new(a1, m1, "All about time", "It sucks but its okay. I like the other ones better")
+ar2 = Article.new(a2, m3, "Make shift", "Its better than the other ones. I prefer the other one sometimes")
+ar3 = Article.new(a3, m4, "Hello", "Very refreshing news on  this one.")
+ar4 = Article.new(a4, m2, "Nostrand", "I don't think this one is my favorite of all the few")
 
+# Author.most_verbose
 
+# a1.articles
+# a3.magazines
 
+# m4.article_titles
+# m1.contributors
 
+# m1.my_author
+# Author.most_active
+# a1.my_article
+# m1.my_article
 
 ### DO NOT REMOVE THIS
 binding.pry
